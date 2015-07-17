6d8a30fb8a59e1196d0b1b41a7ef26bd3e01dca0
Edwin Kestler <info@flatbox.co>
2015-07-17 11:59:58 -0600

    rever index
    changed to Hello world PHP

--- index.php
+++ index.php
@@ -1,24 +1,8 @@
-<?php
-session_start();
-?>
-<!doctype html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
-  <head>
-    <title>Login with facebook</title>
-    <!-- Add login URL in index.php -->
-  <a class="btn btn-block btn-social btn-facebook" href="<?php echo $loginURL; ?>">Login with Facebook</a> 
-  </head>
-  <body>
-  <?php if ($_SESSION['user_id']): ?>
-
-              <!-- Add logout URL in home.php -->
-<a class="btn btn-block btn-social btn-facebook" href="<?php echo $logoutURL; ?>">Logout</a> 
-
-    <?php else: ?>     
-      
-       
-
-
-    <?php endif ?>
-  </body>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?> 
+ </body>
 </html>
