799e5a7f72ef8296184013bcb8a5f794f2bab241
Edwin Kestler <info@flatbox.co>
2015-07-17 11:38:01 -0600

    changed index.php
    added url ligin

--- index.php
+++ index.php
@@ -5,20 +5,18 @@ session_start();
 <html xmlns:fb="http://www.facebook.com/2008/fbml">
   <head>
     <title>Login with facebook</title>
-   --- --- --- 
-  css stuff
-   --- --- ----
+   <!-- Add login URL in index.php -->
+   <a class="btn btn-block btn-social btn-facebook" href="https://www.facebook.com/dialog/oauth?client_id=1492541474313250&redirect_uri=http://flatbox.io/facebook_login.php">Login with Facebook</a> 
  </head>
   <body>
   <?php if ($_SESSION['FBID']): ?>
-        
+
               <!-- Add logout URL in home.php -->
 <a class="btn btn-block btn-social btn-facebook" href="<?php echo $logoutURL; ?>">Logout</a> 
 
     <?php else: ?>     
              
-<!-- Add login URL in index.php -->
-<a class="btn btn-block btn-social btn-facebook" href="<?php echo $loginURL; ?>">Login with Facebook</a>
+
 
     <?php endif ?>
   </body>
