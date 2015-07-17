05dfb872d8ad058345bcd668212568f414173d70
Edwin Kestler <info@flatbox.co>
2015-07-17 15:52:15 -0600

    AddedUSERNAME
    Changed graph API objects.

--- fbconfig.php
+++ fbconfig.php
@@ -4,7 +4,6 @@ session_start();
 // added in v4.0.0
 require_once 'autoload.php';
 require 'functions.php'; // Include functions
-checkuser($fbid,$fbfullname,$femail); // To update local DB 
 
 use Facebook\FacebookSession;
 use Facebook\FacebookRedirectLoginHelper;
@@ -17,10 +16,15 @@ use Facebook\GraphObject;
 use Facebook\Entities\AccessToken;
 use Facebook\HttpClients\FacebookCurlHttpClient;
 use Facebook\HttpClients\FacebookHttpable;
+
+//Start Sesion
+
 // init app with app id and secret
 FacebookSession::setDefaultApplication( '823674294347763','313ca5703fe23ab44173babb859e24bd' );
+
 // login helper with redirect_uri
     $helper = new FacebookRedirectLoginHelper('http://flatbox.io/fbconfig.php' );
+
 try {
   $session = $helper->getSessionFromRedirect();
 } catch( FacebookRequestException $ex ) {
@@ -28,6 +32,7 @@ try {
 } catch( Exception $ex ) {
   // When validation fails or other local issues
 }
+
 // see if we have a session
 if ( isset( $session ) ) {
   // graph api request for user data
@@ -35,17 +40,23 @@ if ( isset( $session ) ) {
   $response = $request->execute();
   // get response
   $graphObject = $response->getGraphObject();
-     	$fbid = $graphObject->getProperty('id');              // To Get Facebook ID
+
+
+     	$fbid = $graphObject->getProperty('id'); // To Get Facebook ID
+        $fbuname = $graphObject->getProperty('username');  // To Get Facebook Username
  	    $fbfullname = $graphObject->getProperty('name'); // To Get Facebook full name
 	    $femail = $graphObject->getProperty('email');    // To Get Facebook email ID
 	/* ---- Session Variables -----*/
-	    $_SESSION['FBID'] = $fbid;           
+	    $_SESSION['FBID'] = $fbid;
+        $_SESSION['USERNAME'] = $fbuname;
         $_SESSION['FULLNAME'] = $fbfullname;
 	    $_SESSION['EMAIL'] =  $femail;
     /* ---- header location after session ----*/
+  checkuser($fbid,$fbfullname,$femail); // To update local DB 
   header("Location: index.php");
 } else {
   $loginUrl = $helper->getLoginUrl();
  header("Location: ".$loginUrl);
 }
+
 ?>
