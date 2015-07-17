62bcb221b6be45dbaf5c4eaa8feaedb59dafae08
Edwin Kestler <info@flatbox.co>
2015-07-16 18:05:06 -0600

    erase fail PHP login
    erase failes

--- dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');    // DB username
-define('DB_PASSWORD', 'FBx_admin2012');    // DB password
-define('DB_DATABASE', 'facebook_users');      // DB name
-$connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die( "Unable to connect");
-$database = mysql_select_db(DB_DATABASE) or die( "Unable to select database");
-?>
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php 
-session_start();
-session_unset();
-    $_SESSION['FBID'] = NULL;
-    $_SESSION['FULLNAME'] = NULL;
-    $_SESSION['EMAIL'] =  NULL;
-header("Location: " ."http://flatbox.io");        // you can enter home page here ( Eg : header("Location: " ."http://flatbox.io"); 
-?>
