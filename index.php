a43e54eeac8292359ce103d876ca27d2581d5d85
Edwin Kestler <info@flatbox.co>
2015-07-17 17:20:56 -0600

    AddingCSShomepage
    Adding CSS Home tutorial

--- index.php
+++ index.php
@@ -3,39 +3,79 @@
 session_start(); 
 ?>
 <!doctype html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
-  <head>
-    <title>Login with Facebook</title>
-<link href="http://www.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet"> 
- </head>
-  <body>
-  <?php if ($_SESSION['FBID']): ?>      <!--  After user login  -->
-<div class="container">
-<div class="hero-unit">
-  <h1>Hello <?php echo $_SESSION['USERNAME']; ?></h1>
-  <p>Welcome to "facebook login" tutorial</p>
-  </div>
-<div class="span4">
- <ul class="nav nav-list">
-<li class="nav-header">Image</li>
-	<li><img src="https://graph.facebook.com/<?php echo $_SESSION['FBID']; ?>/picture"></li>
-<li class="nav-header">Facebook ID</li>
-<li><?php echo  $_SESSION['FBID']; ?></li>
-<li class="nav-header">Facebook fullname</li>
-<li><?php echo $_SESSION['FULLNAME']; ?></li>
-<li class="nav-header">Facebook Email</li>
-<li><?php echo $_SESSION['EMAIL']; ?></li>
-<div><a href="logout.php">Logout</a></div>
-</ul></div></div>
-    <?php else: ?>     <!-- Before login --> 
-<div class="container">
-<h1>Login with Facebook</h1>
-           Not Connected
-<div>
-      <a href="fbconfig.php">Login with Facebook</a></div>
-	 <div> <a href="http://flatbox.io/"  title="Login with facebook">View Post</a>
-	  </div>
-      </div>
-    <?php endif ?>
-  </body>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>CSS3 Animated Navigation Menu</title>
+
+        <!-- Our CSS stylesheet file -->
+        <link rel="stylesheet" href="assets/css/styles.css" />
+
+		<!-- Font Awesome Stylesheet -->
+		<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css" />
+
+		<!-- Including Open Sans Condensed from Google Fonts -->
+		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700,300italic" />
+
+        <!--[if lt IE 9]>
+          <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+    
+    <body>
+
+    	<nav id="colorNav">
+			<ul>
+				<li class="green">
+					<a href="#" class="icon-home"></a>
+					<ul>
+						<li><a href="http://tutorialzine.com/2012/10/css3-dropdown-menu/">Back to the tutorial</a></li>
+						<li><a href="http://tutorialzine.com/2012/10/css3-dropdown-menu/#comments">Get help</a></li>
+						<li><a href="http://tutorialzine.com/2012/10/css3-dropdown-menu/">Download this example</a></li>
+					</ul>
+				</li>
+				<li class="red">
+					<a href="#" class="icon-cogs"></a>
+					<ul>
+						<li><a href="#">Payment</a></li>
+						<li><a href="#">Notifications</a></li>
+						<li><a href="#">Privacy options</a></li>
+						<li><a href="#">Account Settings</a></li>
+					</ul>
+				</li>
+				<li class="blue">
+					<a href="#" class="icon-twitter"></a>
+					<ul>
+						<li><a href="http://twitter.com/Tutorialzine">Follow us on Twitter</a></li>
+						<li><a href="http://www.facebook.com/Tutorialzine">Find us on Facebook</a></li>
+					</ul>
+				</li>
+				<li class="yellow">
+					<a href="#" class="icon-beaker"></a>
+					<ul>
+						<li><a href="http://tutorialzine.com/tag/php/">PHP tutorials</a></li>
+						<li><a href="http://tutorialzine.com/tag/jquery/">jQuery techniques</a></li>
+						<li><a href="http://tutorialzine.com/tag/css/">CSS articles</a></li>
+						<li><a href="http://tutorialzine.com/category/tutorials/">Everything else</a></li>
+					</ul>
+				</li>
+				<li class="purple">
+					<a href="#" class="icon-envelope"></a>
+					<ul>
+						<li><a href="http://tutorialzine.com/contact/">Contact us</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+
+        <footer>
+	        <h2><i>Tutorial:</i> CSS3 Animated Navigation Menu</h2>
+            <a class="tzine" href="http://tutorialzine.com/2012/10/css3-dropdown-menu/">Head on to <i>Tutorial<b>zine</b></i> to download this example</a>
+        </footer>
+
+        <!-- BSA AdPacks code. Please ignore and remove.--> 
+		<script src="http://code.jquery.com/jquery-1.8.2.min.js"></script>
+        <script src="http://cdn.tutorialzine.com/misc/adPacks/v1.js" async></script>
+    
+    </body>
 </html>
