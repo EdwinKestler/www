7f1b66394c7232eb9fb8f468a2946a6fc2b2a450
Edwin Kestler <info@flatbox.co>
2015-07-17 12:04:31 -0600

    added link
    added link

--- index.php
+++ index.php
@@ -3,6 +3,10 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World</p>'; ?>
+
+<!-- Add login URL in index.php -->
+<a class="btn btn-block btn-social btn-facebook" href="<?php echo $loginURL; ?>">Login with Facebook</a>
+ 
  </body>
 </html>
